6c2e4404974e8098ddeccf8db656c67b43c3c70e
Charanjeet Singh <cjeet336@gmail.com>
2025-09-30 14:40:43 +0400

    Completed All api's Get All products, update product, Delete Product

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,36 @@ import { connectDB } from "./config/db.js";
 import Product from "./models/Product.model.js";
 const app = express();
 
-app.use(express.json()) //allow us to accept json data in the req.body
+app.use(express.json()); //allow us to accept json data in the req.body
 dontenv.config();
+
+// API to get all the data frome the database
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("Error while getting all products", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+// API to update the products from the database
+app.put("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = req.body;
+  try {
+    const updateProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    res.status(200).json({ success: true, data: updateProduct });
+  } catch (error) {
+    console.log("Error while updating product", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+// API to add the products into the database
 app.post("/api/products", async (req, res) => {
   // res.send("Server is ready")
   const product = req.body; // request product data from the user
@@ -24,6 +52,19 @@ app.post("/api/products", async (req, res) => {
     res.status(500).json({ success: false, message: "Server Error" });
   }
 });
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted" });
+  } catch (error) {
+    console.log("Error while deleting the product", error.message);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+  console.log("id", id);
+});
+
 app.listen(1000, () => {
   connectDB();
   console.log("Server started at port http://localhost:1000");
