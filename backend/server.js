1da5c5c50600f68e4611bef8a465f2b58c1656b9
Charanjeet Singh <cjeet336@gmail.com>
2025-09-30 14:11:41 +0400

    First API Created to add products

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,30 @@
 import express from "express";
-import dontenv from "dotenv"
+import dontenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/Product.model.js";
 const app = express();
 
-dontenv.config()
-app.get("/products", (req, res)=>
-{
-    res.send("Server is ready")
-})
-app.listen(1000, ()=>
-{
-    connectDB()
-    console.log("Server started at port http://localhost:1000")
-})
+app.use(express.json()) //allow us to accept json data in the req.body
+dontenv.config();
+app.post("/api/products", async (req, res) => {
+  // res.send("Server is ready")
+  const product = req.body; // request product data from the user
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please fill all fields" });
+  }
+  const newProduct = new Product(product);
 
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.log("Error while creating product", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+app.listen(1000, () => {
+  connectDB();
+  console.log("Server started at port http://localhost:1000");
+});
