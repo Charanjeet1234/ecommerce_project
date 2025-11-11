4c31b15619dc49a8c1ac9dd450c5f6862f0dc9f3
Charanjeet Singh <cjeet336@gmail.com>
2025-11-11 15:40:42 +0400

    Added production ready code

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dontenv from "dotenv";
 import cors from 'cors';
 import path from 'path'
 import { fileURLToPath } from "url";
+import helmet from "helmet";
+
 
 import { connectDB } from "./config/db.js";
 import productRoutes from './routes/product.route.js'
@@ -11,6 +13,26 @@ const Port = process.env.PORT || 3000
 app.use(express.json()); //allow us to accept json data in the req.body
 dontenv.config();
 app.use(cors()); // Allows all origins
+
+
+// Set secure headers including CSP
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],       // only allow same origin by default
+        scriptSrc: ["'self'", "'unsafe-inline'"], // allow inline scripts if needed
+        styleSrc: ["'self'", "'unsafe-inline'"],  // allow inline styles
+        imgSrc: ["'self'", "data:", "blob:"],     // allow images from self, base64, blobs
+        connectSrc: ["'self'", "http://localhost:3000"], // allow API requests to backend
+        fontSrc: ["'self'", "https:", "data:"],
+        objectSrc: ["'none'"],        // no <object> tags
+        upgradeInsecureRequests: [],
+      },
+    },
+  })
+);
+
 
 // Fix __dirname for ES Modules
 const __filename = fileURLToPath(import.meta.url);
@@ -21,13 +43,13 @@ app.use("/api/products", productRoutes)
 
 if(process.env.NODE_ENV === "production")
 {
-    app.use(express.static(path.join(__dirname, "/frontend/exommerce-project/dist")))
-    app.get("*", (req, res) =>
+    app.use(express.static(path.join(__dirname, "frontend" , "ecommerce-project" ,"dist")))
+    app.get("/*", (req, res) =>
     {
-      res.sendFile(path.resolve(__dirname, "frontend", "ecommerce-project", "dist", "index.html"))
+      res.sendFile(path.join(__dirname, "frontend", "ecommerce-project" ,"dist", "index.html"))
     })
 }
 app.listen(Port, () => {
   connectDB();
-  console.log("Server started at port http://localhost:" + Port);
+  console.log("Server started at http://localhost:" + Port);
 });
